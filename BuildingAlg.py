75eb44f61e312d221fc3be1d4b51cc96c09ab58e
Paul Ford <applicationsbypaul@gmail.com>
2021-01-16 18:01:26 -0600

    Fix the tostring __str__() then createBuilding() this creates the building sample so we can repeate as neeeded.

--- BuildingAlg.py
+++ BuildingAlg.py
@@ -1,9 +1,9 @@
 ############################################################
-## This program will but an algorithm to determine if the
-## buildings can be seen at sunset from the west. By 
-## default the buildings with 0 are going to be not visible
-## thus being gray. When the building can be seen it will 
-## be green.
+# This program will but an algorithm to determine if the
+# buildings can be seen at sunset from the west. By
+# default the buildings with 0 are going to be not visible
+# thus being gray. When the building can be seen it will
+# be green.
 ############################################################
 
 class Building:
@@ -11,20 +11,44 @@ class Building:
         self.name = name
         self.value = 0
         self.isSeen = False
+
     def showStat(self):
-        print ('-------------')
-        print(self.name)
+        print('-------------')
+        print('Building name: ' + self.name)
         print(self.value)
         print(self.isSeen)
-        print ('-------------')
+        print('-------------')
 
-if __name__ == "__main__":
+    def __str__(self):
+        response = f'------------\n'\
+            'Building Name = {}\n'\
+            'Building Value = {}\n'\
+            'Building isSeen = {}\n'\
+            '------------'\
+            .format(self.name, self.value, self.isSeen)
+        return response
+
+
+def buildingAlg(buildingList):
+    """
+    The algorith to see if 
+    the sun can see the buildings
+    """
+    pass
+
+def createBuildings():
+    """
+    generates a sample of buildings
+    """
     a = Building('a')
     b = Building('b')
     c = Building('c')
     d = Building('d')
     e = Building('e')
-    buildings = [a,b,c,d,e]
-    for value in buildings:
-        value.showStat()
+    buildings = [a, b, c, d, e]
+    return buildings
 
+if __name__ == "__main__":
+    buildingsList = createBuildings()
+    for building in buildingsList:
+        print(building)
